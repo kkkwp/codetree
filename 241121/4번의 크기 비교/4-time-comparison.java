184e321647a48b109b76ad59ea1e914e0c296651
chaeyun3 <chxrryda@gmail.com>
2024-11-21 02:23:14 +0900

    Title: 4번의 크기 비교, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241121/4번의 크기 비교/4-time-comparison.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i=0; i<4; i++) {
+            System.out.println(a > sc.nextInt() ? 1 : 0);
+        }
+    }
+}
