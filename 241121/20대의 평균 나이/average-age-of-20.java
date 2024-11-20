4fe642b427fc36e2dc4320d2567e0c50b4d77966
chaeyun3 <chxrryda@gmail.com>
2024-11-21 02:44:55 +0900

    Title: 20대의 평균 나이, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241121/20대의 평균 나이/average-age-of-20.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int cnt = 0;
+        while (true) {
+            int age = sc.nextInt();
+            if (20 > age || age >= 30) {
+                System.out.printf("%.2f", (double)sum/cnt);
+                break;
+            }
+            sum += age;
+            cnt++;
+        }
+    }
+}
