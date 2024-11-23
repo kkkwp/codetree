64e7d188520c4fa21d47503838535bd444ea2bdf
chaeyun3 <chxrryda@gmail.com>
2024-11-23 23:17:40 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241123/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    static boolean isMasicNumber(int n) {
+        int sum = 0;
+        int tmp = n;
+        while (n > 0) {
+            sum += n%10;
+            n /= 10;
+        }
+        return sum%2==0;
+    }
+
+    static boolean isPrime(int n) {
+        if (n == 1)
+            return false;
+        for (int i=2; i<n; i++)
+            if (n%i==0)
+                return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int cnt = 0;
+        for (int i=a; i<=b; i++)
+            if (isPrime(i) && isMasicNumber(i))
+                cnt++;
+        System.out.print(cnt);
+    }
+}
