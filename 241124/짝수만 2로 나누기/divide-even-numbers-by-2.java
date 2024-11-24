cbabb92ea2b5f6a78b57d09b68d060be27874171
chaeyun3 <chxrryda@gmail.com>
2024-11-24 15:39:58 +0900

    Title: 짝수만 2로 나누기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241124/짝수만 2로 나누기/divide-even-numbers-by-2.java
+++ 241124/짝수만 2로 나누기/divide-even-numbers-by-2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     static void func(int[] arr) {
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<arr.length; i++) {
             if (arr[i] % 2 == 0)
                 arr[i] /= 2;
         }
