29719fd002919582ad420d2598282a44428e3e3e
chaeyun3 <chxrryda@gmail.com>
2024-11-28 02:15:19 +0900

    Title: 이상한 수열, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241128/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    static int recur(int n) {
+        if (n==1)
+            return 1;
+        if (n==2)
+            return 2;
+        return recur((int)Math.floor(n/3)) + recur(n-1);
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(recur(n));
+    }
+}
