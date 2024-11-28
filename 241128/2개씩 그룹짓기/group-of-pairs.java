22b84a9ae9c15ff1c85dd1ed782886f49e3f23d9
chaeyun3 <chxrryda@gmail.com>
2024-11-28 16:07:08 +0900

    Title: 2개씩 그룹짓기, Time: 173ms, Memory: 12MB, Status: Passed - CodeTree

--- 241128/2개씩 그룹짓기/group-of-pairs.java
+++ 241128/2개씩 그룹짓기/group-of-pairs.java
@@ -1,25 +1,22 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
-    static int n, answer;
-    static int[] arr;
-    
-    static void findMax(int idx) {
-        int sum = arr[idx] + arr[n-1-idx];
-        answer = Math.max(answer, sum);
-    }
-
+public class Main {   
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        arr = new int[n];
-        for (int i=0; i<n; i++)
+        int n = sc.nextInt();
+        int[] arr = new int[2*n];
+        for (int i=0; i<2*n; i++)
             arr[i] = sc.nextInt();
+
         Arrays.sort(arr);
-        for(int i=0; i<n; i++)
-            findMax(i);
+
+        int answer = 0;
+        for(int i=0; i<n; i++) {
+            int sum = arr[i] + arr[2*n-1-i];
+            answer = Math.max(answer, sum);
+        }  
         System.out.print(answer);
     }
 }
