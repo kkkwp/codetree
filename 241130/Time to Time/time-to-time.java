c271d34d6f7618da7f0cb5f972d42f245035530b
chaeyun3 <chxrryda@gmail.com>
2024-11-30 04:06:23 +0900

    Title: Time to Time, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241130/Time to Time/time-to-time.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        System.out.print((c*60+d) - (a*60+b));
+    }
+}
