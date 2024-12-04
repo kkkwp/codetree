c4d9f133b57e31025c48d2dee4bc7b94a21e8ab1
chaeyun3 <chxrryda@gmail.com>
2024-12-04 23:01:36 +0900

    Title: 만나는 그 순간, Time: 228ms, Memory: 21MB, Status: Passed - CodeTree

--- 241204/만나는 그 순간/the-moment-we-meet.java
+++ 241204/만나는 그 순간/the-moment-we-meet.java
@@ -1,46 +1,45 @@
 import java.util.Scanner;
 
 public class Main {
+    static final int MAX_TIME = 1_000_000;
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        int totalTime = 0;
-
-        int[] a = new int[1_000_000];
-        int cur = 0;
-        int time = 0;
+        int[] a = new int[MAX_TIME+1];
+        int cur = 1;
         while (n-- > 0) {
             char d = sc.next().charAt(0);
             int t = sc.nextInt();
             int dir = d == 'L' ? -1 : 1;
-            for (int i=0; i<t; i++) {
-                cur += dir;
-                a[time++] = cur;
+            while (t-- > 0) {
+                a[cur] = a[cur-1] + dir;
+                cur++;
             }
         }
 
-        int[] b = new int[1_000_000];
-        cur = 0;
-        time = 0;
+        int[] b = new int[MAX_TIME+1];
+        cur = 1;
         while (m-- > 0) {
             char d = sc.next().charAt(0);
             int t = sc.nextInt();
             int dir = d == 'L' ? -1 : 1;
-            for (int i=0; i<t; i++) {
-                cur += dir;
-                b[time++] = cur;
+            while (t-- > 0) {
+                b[cur] = b[cur-1] + dir;
+                cur++;
             }
         }
 
-        for (int i=0; i<time; i++) {
+        int answer = -1;
+        for (int i=1; i<cur; i++) {
             if (a[i] == b[i]) {
-                System.out.print(i+1);
-                return;
+                answer = i;
+                break;
             }
         }
-        System.out.print(-1);
+        System.out.print(answer);
     }
 }
